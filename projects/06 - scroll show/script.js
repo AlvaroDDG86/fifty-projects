032fbb26c3a0ca560a0786f13940ea486a4fcb65
adios <adios@minsait.com>
2021-03-04 12:03:08 +0100

    sixty project

---
+++ projects/06 - scroll show/script.js
@@ -0,0 +1,20 @@
+(function() {
+    const boxes = document.querySelectorAll('.box')
+    window.addEventListener('scroll', checkBoxes)
+
+    checkBoxes()
+
+    function checkBoxes() {
+        const trigger = (window.innerHeight / 5 * 4) + 110
+        boxes.forEach(box => {
+            const boxTop = box.getBoundingClientRect().top
+
+            if (boxTop < trigger) {
+                box.classList.add('show')
+            } else {
+                box.classList.remove('show')
+            }
+        })
+
+    }
+}())
